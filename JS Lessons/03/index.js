538d67e32f05e6ad17c44a0e6124b6b6d0920f3a
BenyaChef <benyachef@gmail.com>
2022-01-26 22:22:02 +0200

    for-if-else

--- JS Lessons/03/index.js
+++ JS Lessons/03/index.js
@@ -1,36 +1,36 @@
 
-    var input1El = document.getElementById('input1');
-    var input2El = document.getElementById('input2');
-    var input3El = document.getElementById('input3');
-    var myHeadEl = document.getElementById('myHead');
-    var aboutMeEl = document.getElementById('aboutMe');
+var input1El = document.getElementById('input1');
+var input2El = document.getElementById('input2');
+var input3El = document.getElementById('input3');
+var myHeadEl = document.getElementById('myHead');
+var aboutMeEl = document.getElementById('aboutMe');
 
- /*
- input1El.className = 'error';
- input2El.className = 'error';
- input3El.className = 'error';
+/*
+input1El.className = 'error';
+input2El.className = 'error';
+input3El.className = 'error';
 
- /*
- input1El = newValue;
- input2El = newValue;
- input3El = newValue;
- */
+/*
+input1El = newValue;
+input2El = newValue;
+input3El = newValue;
+*/
 
 function setMyNameToAllInputs() {
     var newValue = 'Max';
-input1El.value = newValue;
-input2El.value = newValue;
-input3El.value = newValue;
+    input1El.value = newValue;
+    input2El.value = newValue;
+    input3El.value = newValue;
 }
 
 function setMyLastNameToAllInputs() {
     var newValue = 'Bineckiy';
-input1El.value = newValue;
-input2El.value = newValue;
-input3El.value = newValue;
+    input1El.value = newValue;
+    input2El.value = newValue;
+    input3El.value = newValue;
 
 }
- 
+
 
 function changeValue(newValue) {
 
@@ -45,7 +45,7 @@ function setError(elemetParameter) {
     elem.className = 'error';
 }
 
-function timeOutError(){
+function timeOutError() {
     setError(aboutMeEl);
     setError(input1El);
 }
@@ -65,28 +65,28 @@ setError(myHeadEl);
 setError(aboutMeEl);*/
 
 //setTimeout(timeOutError, 4000,);//
- 
+
 /*function myName(){
     alert('Maxim');
 }
 
 setTimeout(myName, 3000);
 */
-function hideElement(elementParametr){
+function hideElement(elementParametr) {
     var elem = document.getElementById(elementParametr);
     elem = elementParametr;
     elem.className = 'hide';
 }
 
-function showElement(elementParametr){
+function showElement(elementParametr) {
     var elem = document.getElementById(elementParametr);
     elem = elementParametr;
     elem.className = 'show';
 }
 
-function intervalTimer(){
+function intervalTimer() {
     var elem = document.getElementById('input1');
-    elem.value = Number(elem.value)+1;
+    elem.value = Number(elem.value) + 1;
 }
 
 hideElement(input2El);
@@ -95,22 +95,22 @@ showElement(input2El);
 
 setInterval(intervalTimer, 1000);
 
-function deletElement(elemetParameter){
+function deletElement(elemetParameter) {
     var element = document.getElementById(elemetParameter);
-        element = elemetParameter;
+    element = elemetParameter;
     if (element != null) {
         element.remove();
-        }
-   
+    }
+
 }
 
 //deletElement(aboutMeEl);
 
-function changeClassName(id, className){
+function changeClassName(id, className) {
     var element = document.getElementById(id, className);
     element = id;
     if (element != null) {
-    element.className = className;
+        element.className = className;
     }
 }
 
@@ -119,16 +119,16 @@ changeClassName(aboutMeEl, 'hide-element');
 
 var arrayFinde = [input1El, input2El, input3El, myHeadEl, aboutMeEl];
 
-for (var i = 0; i < arrayFinde.length; i++) {
-     if (arrayFinde[i].value === "2") {
-        arrayFinde[i].remove();
-     }
+function elementRemove(valueForDelete ) {
+    
+    for (var i = 0; i < arrayFinde.length; i++) {
+        if (arrayFinde[i].value === valueForDelete) {
+            arrayFinde[i].remove();
+        } else if (arrayFinde[i].value === valueForDelete) {
+            arrayFinde[i].remove();
+        }
 
-}
-
-for (var i = 0; i < arrayFinde.length; i++) {
-    if (arrayFinde[i].value === "3") {
-       arrayFinde[i].remove();
     }
-
 }
+
+elementRemove("");

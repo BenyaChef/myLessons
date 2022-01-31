4609bbf7c3602051d9238c22c6fde70165bb904a
BenyaChef <benyachef@gmail.com>
2022-01-31 19:26:12 +0200

    lesson

--- full-js-course/js/script.js
+++ full-js-course/js/script.js
@@ -1,15 +1,11 @@
 "use strict";
- //alert('Hello!');
+ let incer = 10,
+     decre = 15;
 
-// const result = confirm('Are you here?');
-// console.log(result);
+incer++;
+decre--;
+console.log(incer);
+console.log(decre);
 
-//const answer = prompt('Вам есть 18?', '18');
-
-const answers = [];
-
- answers[0] = prompt('Как ваше имя?', '');
- answers[1] = prompt('Как ваше фамилия?', '');
- answers[2] = prompt('Сколько вам лет?', '');
-
-console.log(answers);
+const isChecked = true,
+      isClossed = true;

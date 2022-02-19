6976c99755e1819290b5db8f1bd7405da9b06522
Maxim <benyachef@gmail.com>
2022-02-19 20:23:50 +0200

    lesson

---
+++ full-js-course/js/touch.js
@@ -0,0 +1,18 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+
+        console.log(e.targetTouches[0].pageX);
+        
+    });
+
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
+
+        console.log('end');
+    });
+});

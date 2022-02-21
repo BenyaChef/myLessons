4f6f25dda2e63c971f9d2828e5116bbe8857846c
Maxim <benyachef@gmail.com>
2022-02-21 20:19:59 +0200

    lesson 40

---
+++ full-js-course/js/40.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const now = new Date();
+
+    // console.log(now.getFullYear());
+    // console.log(now.getMonth());
+    // console.log(now.getDate());
+    // console.log(now.getDay());
+    // console.log(now.getUTCHours());
+
+
+    // console.log(now.getTimezoneOffset());
+
+    let start = new Date();
+
+    for (let i = 0; i < 100000; i++) {
+        let some = i ** 3;
+    }
+
+    let end = new Date();
+    
+    alert(`Цикл отработал за ${end - start} миллисекунд`);

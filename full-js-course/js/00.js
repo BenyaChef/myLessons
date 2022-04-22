75803374c9ff76e67a59ef546742e04706a9bd27
Maxim <benyachef@gmail.com>
2022-04-22 15:12:50 +0300

    homeWork

--- full-js-course/js/00.js
+++ full-js-course/js/00.js
@@ -1,4 +1,10 @@
 'use strict';
 
-let number = 5;   
-const leftBorderWidht = 1;
+const storeName = 'MyStore';
+
+const storeDescription = {
+    budget: 10000,
+    employees: ['Max','Alena','Vika'],
+    products: {apple: 10, wather: 1},
+    open: true
+};

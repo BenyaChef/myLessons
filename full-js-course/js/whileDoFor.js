e656f3868bd67069bc3270f57cb788e3f733cf2e
Maxim <benyachef@gmail.com>
2022-04-29 13:50:48 +0300

    cycles

---
+++ full-js-course/js/whileDoFor.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let num = 50;
+
+// while (num <= 55) { //условие!
+//     console.log(num);// пока условие не выполнено выполняй такие действия!
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+}

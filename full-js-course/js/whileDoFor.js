7c8e99f283ad10bf7981dda30597bce7cd267400
Maxim <benyachef@gmail.com>
2022-05-02 20:05:47 +0300

    commit

--- full-js-course/js/whileDoFor.js
+++ full-js-course/js/whileDoFor.js
@@ -20,3 +20,37 @@ for (let i = 1; i <= 11; i++) {
     console.log(num);
     num--;
 }
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+let i = 2;
+while ( i <= 16) {
+    if (i % 2 === 0) {
+    } else {
+        console.log(i);
+    }
+    i++;
+}
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
+}

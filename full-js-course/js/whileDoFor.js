271cff8dc2dae6b30891f1d1d75457a6abc87571
Maxim <benyachef@gmail.com>
2022-05-03 13:13:25 +0300

    home work

--- full-js-course/js/whileDoFor.js
+++ full-js-course/js/whileDoFor.js
@@ -54,3 +54,13 @@ function fifthTask() {
     console.log(arrayOfNumbers);
     return arrayOfNumbers;
 }
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
+}
+
+console.log(result);
+

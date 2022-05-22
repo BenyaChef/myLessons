75f0d90312bdeb838b9a6e634ebf3e4e38ed1283
Maxim <benyachef@gmail.com>
2022-05-22 19:52:07 +0300

    homeWork

---
+++ full-js-course/js/cikle01.js
@@ -0,0 +1,25 @@
+'use strict';
+// Вложиные циклы 
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result)

f10f9fd840774e7d9866e6f167c7a165a65e88cf
Maxim <benyachef@gmail.com>
2022-05-23 19:54:48 +0300

    I dont know

--- full-js-course/js/function.js
+++ full-js-course/js/function.js
@@ -32,3 +32,39 @@ function test() {
 }
 
 test();
+
+function sayHello (name) {
+    return `Привет, ${name}!`;
+}
+
+sayHello('Максим');
+
+
+function returnNeighboringNumbers(num) {
+    return [num -1, num, num + 1];
+}
+
+returnNeighboringNumbers(4);
+
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+getMathResult(10, 5);
